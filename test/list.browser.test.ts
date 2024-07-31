a7eb4d3b7f26ccbae036dd2bd4eba3fd18932c0d
Eric Mika <eric@ericmika.com>
2024-07-31 13:48:29 -0400

    Fix possibly uninitialized list test.

--- test/list.browser.test.ts
+++ test/list.browser.test.ts
@@ -1,8 +1,32 @@
-import { listNotes } from '../src/lib'
+import { type YankiNote, listNotes, syncNotes } from '../src/lib'
 import { expect, it } from 'vitest'
 
 it('lists notes', async () => {
-	const notes = await listNotes({ namespace: '*' })
+	// Mock data
+	const namespace = 'Yanki Test - list.browser.test'
+	const testNote: YankiNote = {
+		deckName: 'test-minimal-notes',
+		fields: {
+			Back: 'Bye',
+			Front: 'Hi',
+			YankiNamespace: namespace,
+		},
+		modelName: 'Yanki - Basic',
+		noteId: undefined,
+		tags: [],
+	}
 
+	await syncNotes([testNote], {
+		namespace,
+	})
+
+	const notes = await listNotes({ namespace })
 	expect(notes.notes.length).toBeGreaterThan(0)
+
+	await syncNotes([], {
+		namespace,
+	})
+
+	const noNotes = await listNotes({ namespace })
+	expect(noNotes.notes.length).toBe(0)
 })
